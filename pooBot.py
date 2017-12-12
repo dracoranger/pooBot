d3d2005d49bad2f5802ce06ee1bbaff5803d9e29
Bowers <1501539992@mil>
2017-12-12 11:38:48 -0500

    v0.5
    mostly outlined

--- pooBot.py
+++ pooBot.py
@@ -4,9 +4,12 @@ import asyncio
 import re
 from datetime import datetime
 from datetime import timedelta
-from threading import Timer
+import math
+import statistics
+import random
 import discord
 from discord.ext import commands
+
 #import logging
 
 client = discord.Client()
@@ -17,20 +20,32 @@ BOT_FOLDER=""
 config = open('config.txt','r')
 conf = config.readlines() #push to array or do directly
 token = conf[0][:-1]
-print(token)
 channelNum = conf[1][:-1]
 userComp = conf[2][:-1]
 
 '''
 Calculate probability
+"2016-03-24T23:15:59.605000+00:00"
 '''
-def generateEstimate(previousPoos):
+def generateEstimate():
     #Previous poos = list of "timestamp": "2016-03-24T23:15:59.605000+00:00"
+    global channelNum
     timeDifference = []
     last = 0
-    for i in previousPoos:
-        if last == 0:
-
+    for message in client.logs_from(channelNum, limit=500):
+        if not message.author == client.user:
+            if last == 0:
+                last = message.timestamp
+            else:
+                timeDif = message.timestamp - last#convert time string into time num
+                timeDifference.append(timeDif)
+    timeDifference = sorted(timeDifference)
+    median = timeDifference[math.floor(len(timeDifference)/2)]
+    stdev = statistics.stdev(timeDifference)
+    upperBound = median * stdev
+    lowerBound = median - (upperBound - median)
+    ret = "Aaron uses the bathroom every "+ str(median) +"units with a standard deviation of "+ str(stdev)+"\nThe upper bound for the next one is " +str(upperBound)+", the lower bound is "+ str(lowerBound)+". \nI predict "+str(random.randrange(lowerBound,upperBound,1))+'.'
+    return ret
 
 @client.event
 async def on_ready():
@@ -45,94 +60,12 @@ async def on_message(message):
     global channelNum
     if message.author == client.user:
         return
-    if message.channel == client.get_channel(channelNum) or message.channel.is_private:
-
-
+    if message.channel == client.get_channel(channelNum) and not message.author == client.user:
         '''
         Post result
         '''
-        '''
-        prints the list of keys
-        '''
-        if message.content.startswith('!keylist'):
-            keys = open(keysName, 'r')
-            keylist = keys.readlines()
-            keys.close()
-            temp = ''
-            for i in keylist:
-                temp = temp+i.split(',')[0]+'\n'#should only show name
-            if temp == '':
-                temp = 'No keys in storage'
-            await client.send_message(message.channel, temp)
-        '''
-        prints all commands
-        '''
-        if message.content.startswith('!help'):
-            keylist = "!keylist = prints a list of games that have keys, works in either server or in pms"
-            gib = "!gib [gameName] [key]= gives a key to the bot, only works in pms"
-            take = "!take [gameName] = messages you with the game's key, works only in server, recieve key in pm, message posted to server"
-            ret = keylist+'\n'+gib+'\n'+take+'\n'
-            await client.send_message(message.channel, ret)
-    if message.channel == client.get_channel(channelNum):
-        '''
-        gives user a key
-        '''
-        global keyTakenToday
-        if message.content.startswith('!take'):
-            if not message.author in keyTakenToday:
-                item = message.content[6:]
-                keys = open(keysName, 'r+')
-                keylist = keys.readlines()
-                keys.close()
-                temp = ''
-                gib = ''
-                gibPerm = ''
-                for i in keylist:
-                    if i.split(',')[0].upper() == item.upper():
-                        if gib == '':
-                            gibPerm = i
-                            gib = 'Game: ' + i.split(',')[0]+ ' Key: ' + i.split(',')[1] + ' Given by: '+ i.split(',')[2]
-                        else:
-                            temp = temp+i
-                    else:
-                        temp = temp+i
-                if gib == '':
-                    publicMessage = "Item requested is not avalible"
-                    gib = "Not avalible.  Please tell Draco if this is wrong"
-                else:
-                    publicMessage = message.author.name + " has claimed " + gibPerm.split(',')[0] + ' which was donated by ' + gibPerm.split(',')[2]
-                await client.send_message(message.author,gib)
-                await client.send_message(client.get_channel(channelNum), publicMessage)
-                addToUsed = open(usedKeys, 'a')
-                addToUsed.write(gibPerm)
-                addToUsed.close()
-                a = open(keysName, 'w')
-                a.write(temp)
-                a.close()
-                keyTakenToday.append(message.author)
-            else:
-                await client.send_message(message.author,"Sorry, due to potential security issues, we're limiting the number of keys taken to 1 per day")
-
-    if message.channel.is_private:
-        '''
-        takes a key from a user
-        '''
-        if message.content.startswith('!gib'):
-            item = message.content[4:]
-            temp = item.split(' ')
-            name = ''
-            comp = re.compile(r'(\w\w\w\w\w\-\w\w\w\w\w\-\w\w\w\w\w\-\w\w\w\w\w\-\w\w\w\w\w)|(\w\w\w\w\w\-\w\w\w\w\w\-\w\w\w\w\w)')#and third one?
-            co = comp.match(temp[len(temp)-1])
-            if len(temp) > 2: # and len(co)>0:
-                for i in range(0, len(temp)-1):
-                    name = name+temp[i]
-                key = temp[len(temp)-1]
-                await client.send_message(message.author, "Thank you!\n I recieved "+name+" with a key of "+ key)
-                a = open(keysName, 'a')
-                a.write(name + ',' + key + ',' + message.author.name + '\n')
-                a.close()
-            else:
-                await client.send_message(message.author, "I think your game might be missing a key")
+        temp = generateEstimate()
+        await client.send_message(message.channel, temp)
     '''
         {
     "id": "162701077035089920",
